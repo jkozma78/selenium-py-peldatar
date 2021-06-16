131ae500927453adab1e17c3c31b119975cbeff2
Kozma János <jkozma@freestart.hu>
2021-06-16 13:13:43 +0200

    substract.py feledat beadása

---
+++ python-homework/substract.py
@@ -0,0 +1,4 @@
+kisebbitendo = float(input("Kérem a kisebbítendő számot megadni:"))
+kivonando = float(input("Kérem a kivonandó számot megadni:"))
+kulonbseg=kisebbitendo-kivonando
+print("Különbség:", kulonbseg)
