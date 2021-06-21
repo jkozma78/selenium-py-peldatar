60f82c3a47803d6dc4223d8865ed1d9f20a33aff
Kozma János <jkozma@freestart.hu>
2021-06-21 21:05:31 +0200

    feladatok feltöltése

---
+++ selenium-homework/elementtype.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+
+driver = webdriver.Edge("D:\edge\msedgedriver.exe")
+
+try:
+    driver.get("http://localhost:9999/trickyelements.html")
+    element = driver.find_elements_by_xpath("//button[contains(@id, 'element')]")
+    element[0].click()
+    assert f'{element[0].text} was clicked' == driver.find_element_by_id("result").text
+
+except IndexError:
+    print("Nem található button type element")
+
+finally:
+    driver.close()
